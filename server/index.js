39a124587a67c1348b13cde81d38fa89e8cc9b72
deep <vagheladeep2412@gmail.com>
2023-02-28 12:59:04 +0530

    First commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const {Server} = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"],
+    },
+});
+
+io.on("connection",(socket)=>{
+    // console.log(`User connected: ${socket.id}`);
+
+    socket.on("send_msg",(data)=>{
+        socket.broadcast.emit("rcv_msg",data);
+    })
+})
+
+
+server.listen(3001,()=> console.log("server is running..."));
